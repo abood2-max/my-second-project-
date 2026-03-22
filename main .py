b1bc30d1c25310e1a526902f6adb2cad9d83450e
Abdel Rahman Rami E’Mar <abood752006@icloud.com>
2026-03-22 21:02:40 +0300

    Create main .py

---
+++ main .py
@@ -0,0 +1,23 @@
+# print first sentence 
+print (" Welcome to pyton ")
+#print second sentence 
+#print (" Wello beprogrammer.jo")
+#####y = 3
+#print(x + y)
+# the second  operation 
+#e=8
+#z=9
+#print (e*z)
+#the thrid  operation 
+#q=10
+#s=8
+#print (q-s)
+#the fourth operation 
+n=10
+m=2
+print (n/m )
+name = "abood "
+print ("Welcome " + name )
+age = 20 
+print (" my name is "+name + " and my age is " + str(age)  )
+print ("abood ")
