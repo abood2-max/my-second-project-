09e85ead413a916b2dfe0764a479da92191e537a
Abdel Rahman Rami E’Mar <abood752006@icloud.com>
2026-03-22 21:08:55 +0300

    Uncomment code for various operations
    Uncommented print statements and variable assignments for operations.

--- main .py
+++ main .py
@@ -1,17 +1,17 @@
-# print first sentence 
+ #print first sentence 
 print (" Welcome to pyton ")
 #print second sentence 
-#print (" Wello beprogrammer.jo")
-#####y = 3
-#print(x + y)
+print (" Wello beprogrammer.jo")
+y = 3
+print(x + y)
 # the second  operation 
-#e=8
-#z=9
-#print (e*z)
+e=8
+z=9
+print (e*z)
 #the thrid  operation 
-#q=10
-#s=8
-#print (q-s)
+q=10
+s=8
+print (q-s)
 #the fourth operation 
 n=10
 m=2
